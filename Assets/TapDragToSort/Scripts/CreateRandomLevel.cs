1c27eaffb3aaae076fcad46aee5190288f21da44
bariskes11 <kesdesng@gmail.com>
2021-06-04 21:02:05 +0300

    General Scene and prefab creations

---
+++ Assets/TapDragToSort/Scripts/CreateRandomLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRandomLevel : MonoBehaviour
+{
+    // anypossible gameobject in box
+    [Tooltip("Any possible gameobject in box")]
+    public GameObject[] ObjectTypes;
+    // totalboxCount in the game
+    [Tooltip("total Box Count in the game")]
+    public int boxCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int Len = ObjectTypes.Length;
+        if (Len == 0)
+        {
+            Debug.Log(" Please Add GameObjects on 'ObjectTypes'");
+            return;
+        }
+
+
+    }
+
+   
+}
